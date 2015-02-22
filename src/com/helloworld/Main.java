327e1b8e6ee225b7ed96f5e993d43c79cab1c94d
CPBach <deathrowstevie@hotmail.de>
2015-02-22 15:59:05 +0100

    Added first java-source file

---
+++ src/com/helloworld/Main.java
@@ -0,0 +1,14 @@
+package com.helloworld;
+
+public class Main {
+
+	public static void main(int argc, String[] argv){
+		sayHello();
+	}
+	
+	
+	public static void sayHello(){
+		System.out.println("Hello");
+	}
+
+}
